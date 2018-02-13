7b5448524036f6487c4d68880d616ceecf533ab2
highjump <lixiang901112@163.com>
2018-02-14 00:02:17 +0800

    Added UI for splash screen

---
+++ app/src/main/java/com/brainyapps/e2fix/activities/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.brainyapps.e2fix.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.brainyapps.e2fix.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
