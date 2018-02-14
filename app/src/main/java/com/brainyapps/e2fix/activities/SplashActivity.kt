2fd1810f3fe5f130c30a3faec4ced5eee9e7e70a
highjump <lixiang901112@163.com>
2018-02-14 14:35:05 +0800

    Added UI for sign in page

--- app/src/main/java/com/brainyapps/e2fix/activities/SplashActivity.kt
+++ app/src/main/java/com/brainyapps/e2fix/activities/SplashActivity.kt
@@ -1,13 +1,27 @@
 package com.brainyapps.e2fix.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.brainyapps.e2fix.R
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, SPLASH_TIME_OUT.toLong())
     }
 }
