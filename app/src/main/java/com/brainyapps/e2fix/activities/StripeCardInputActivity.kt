02a4481c71d3348f5e625d297d174cb89c7e95d9
highjump <lixiang901112@163.com>
2018-03-13 00:05:55 +0800

    Added UI for payment detail

---
+++ app/src/main/java/com/brainyapps/e2fix/activities/StripeCardInputActivity.kt
@@ -0,0 +1,13 @@
+package com.brainyapps.e2fix.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.brainyapps.e2fix.R
+
+class StripeCardInputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stripe_card_input)
+    }
+}
